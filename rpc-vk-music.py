230d151d780d5a52e231dcfce49dc4aff1758be9
menleev <110269230+menleev@users.noreply.github.com>
2022-11-23 02:42:28 +0300

    try)))

--- rpc-vk-music.py
+++ rpc-vk-music.py
@@ -5,25 +5,22 @@ from bs4 import BeautifulSoup
 import os
 
 id = 994345924761489510
-RPC = Presence(id)
 user_id = None
 
 def menu():
     os.system("cls")
     global user_id
+    global pid
     user_name = os.getlogin()
     while True:
         try:
             if not os.path.exists("C:\\Users\\" + user_name + "\\AppData\\Roaming\\vk_rpc"):
-                #создаем папку в AppData\Roaming
                 os.mkdir("C:\\Users\\" + user_name + "\\AppData\\Roaming\\vk_rpc")
                 continue
-            #если файл с id нет, то создаем его
             if not os.path.exists("C:\\Users\\" + user_name + "\\AppData\\Roaming\\vk_rpc\\id.txt"):
                 file = open("C:\\Users\\" + user_name + "\\AppData\\Roaming\\vk_rpc\\id.txt", "w")
                 file.close()
                 continue
-            #если файл с id пустой, то запрашиваем id
             if os.stat("C:\\Users\\" + user_name + "\\AppData\\Roaming\\vk_rpc\\id.txt").st_size == 0:
                 user_id = input("Введите id страницы: ")
                 file = open("C:\\Users\\" + user_name + "\\AppData\\Roaming\\vk_rpc\\id.txt", "w")
@@ -41,27 +38,29 @@ def menu():
             time.sleep(1)
             continue
             
-
 def get_song():
-        response = requests.get("https://vk.com/id" + str(user_id))
-        soup = BeautifulSoup(response.text, "html.parser")
-        status = soup.find("div", {"class": "pp_status"})
-        if status is None:
-            return None
-        else:
-            status = status.text
-            # получаем название песни
-            song = status[status.find("«") + 1:status.find("»")]
-            # получаем имя исполнителя
-            artist = status[status.find("»") + 1:status.find("—") - 1]
-            # получаем название альбома
-            album = status[status.find("—") + 1:]
-            url = "https://www.google.com/search?q=" + status + "&tbm=isch"
-            response = requests.get(url)
+    while True:
+        try:
+            response = requests.get("https://vk.com/id" + str(user_id))
             soup = BeautifulSoup(response.text, "html.parser")
-            images = soup.find_all("img")
-            image = images[1]['src']
-            return song, artist, album, image
+            status = soup.find("div", {"class": "pp_status"})
+            if status is None:
+                return None
+            else:
+                status = status.text
+                song = status[status.find("«") + 1:status.find("»")]
+                artist = status[status.find("»") + 1:status.find("—") - 1]
+                album = status[status.find("—") + 1:]
+                url = "https://www.google.com/search?q=" + status + "&tbm=isch"
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, "html.parser")
+                images = soup.find_all("img")
+                image = images[1]['src']
+                return song, artist, album, image
+        except Exception as e:
+            print("da")
+            os.system("cls")
+            time.sleep(1)
 
 def update():
     while True:
@@ -69,7 +68,7 @@ def update():
             if get_song() != False:
                 song, artist, album, image = get_song()
                 RPC.update(state=artist, details=album, large_image=image, large_text=song)
-                time.sleep(10)
+                time.sleep(5)
                 os.system("cls")
                 print("Статус обновлен")
                 continue
@@ -81,11 +80,18 @@ def update():
                 continue
         except Exception as e:
             os.system("cls")
+            RPC.clear()
             print("ВКЛЮЧИТЕ -Трансляция аудиозаписей- К СЕБЕ НА СТРАНИЦУ!!!")
             time.sleep(1)
             continue
-
-
+        
 if __name__ == "__main__":
-    RPC.connect()
-    menu()
+    while True:
+        try:
+            RPC = Presence(id)
+            RPC.connect()
+            menu()
+        except Exception as e:
+            os.system("cls")
+            print("Запустите дискорд!!!!!!!!!!")
+            time.sleep(1)
